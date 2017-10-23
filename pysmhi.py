895df38bf027556f87d3e02ec12858e15735d1a7
Mattias Jonsson <xdr@xdr.nu>
2017-10-23 23:49:31 +0200

    Reverted to a working condition.

--- pysmhi.py
+++ pysmhi.py
@@ -12,7 +12,13 @@ with open('last_position.json', 'r+') as json_file:
     last_position = json.load(json_file)
     json_file.close()
 
-if (current_milli_time() - last_position['last_checked']) > 9000:
+if last_position['last_checked'] == 0:
+    last_position['last_checked'] = current_milli_time()
+    json_file = open("last_position.json", 'w+')
+    json_file.write(json.dumps(last_position))
+    json_file.close()
+
+if (current_milli_time() - last_position['last_checked']) >= 3600000:
 
     current_location = json.load(urllib2.urlopen(location_url))
     print current_milli_time() - last_position['last_checked']
@@ -24,32 +30,11 @@ if (current_milli_time() - last_position['last_checked']) > 9000:
     last_position['country_name'] = current_location['country_name']
     last_position['longitude'] = current_location['longitude']
     last_position['latitude'] = current_location['latitude']
+
+
     json_file = open("last_position.json", 'w+')
     json_file.write(json.dumps(last_position))
     json_file.close()
-
-    weather_url = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/" + str(last_position['longitude']) + "/lat/" + str(last_position['latitude']) +"/data.json"
-    current_weather = json.load(urllib2.urlopen(weather_url))
-
-    with open('weather_desc.json', 'r+') as json_file:
-        weather_desc = json.load(json_file)
-    json_file.close()
-
-    print('Updated ' + current_weather['approvedTime'][11:16] + ' ' + current_weather['approvedTime'][0:10])
-    #Get next 48h of weather present only forcasts for mid day and night.
-    for x in range (0, 48):
-        if current_weather['timeSeries'][x]['validTime'][11:16]=='12:00' or current_weather['timeSeries'][x]['validTime'][11:16] == '00:00':
-            print('  --------------------------------------  ')
-            print(location_city)
-            print ('Date: ' + current_weather['timeSeries'][x]['validTime'][0:10])
-            print('Time: ' + current_weather['timeSeries'][x]['validTime'][11:16])
-
-        #  Get symbols for the weather.
-        for weather in weather_desc['weathers']:
-            if current_weather['timeSeries'][x]['parameters'][18]['values'][0] == weather['value']:
-                print('Weather: ' + weather['name'] + '  ' + weather['symbol'])
-                print('Temperature: ' +  str(current_weather['timeSeries'][x]['parameters'][11]['values'][0])) + ' C '
-
 
 else:
     with open('last_position.json', 'r+') as json_file:
@@ -63,24 +48,24 @@ else:
     location_long = str(location_long)
     json_file.close()
 
-if (current_milli_time() - last_position['last_checked']) > 9000:
-    weather_url = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/" + location_long + "/lat/" + location_lat +"/data.json"
-    current_weather = json.load(urllib2.urlopen(weather_url))
 
-    with open('weather_desc.json', 'r+') as json_file:
-        weather_desc = json.load(json_file)
-    json_file.close()
+weather_url = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/" + location_long + "/lat/" + location_lat +"/data.json"
+current_weather = json.load(urllib2.urlopen(weather_url))
 
-    print('Updated ' + current_weather['approvedTime'][11:16] + ' ' + current_weather['approvedTime'][0:10])
-    #Get next 48h of weather present only forcasts for mid day and night.
-    for x in range (0, 48):
-        if current_weather['timeSeries'][x]['validTime'][11:16]=='12:00' or current_weather['timeSeries'][x]['validTime'][11:16] == '00:00':
-            print('  --------------------------------------  ')
-            print(location_city)
-            print ('Date: ' + current_weather['timeSeries'][x]['validTime'][0:10])
-            print('Time: ' + current_weather['timeSeries'][x]['validTime'][11:16])
+with open('weather_desc.json', 'r+') as json_file:
+    weather_desc = json.load(json_file)
+json_file.close()
 
-        #  Get symbols for the weather.
+print('Updated ' + current_weather['approvedTime'][11:16] + ' ' + current_weather['approvedTime'][0:10])
+# Get next 48h of weather present only forcasts for mid day and night.
+for x in range (0, 48):
+    if current_weather['timeSeries'][x]['validTime'][11:16]=='12:00' or current_weather['timeSeries'][x]['validTime'][11:16] == '00:00':
+        print('  --------------------------------------  ')
+        print(location_city)
+        print ('Date: ' + current_weather['timeSeries'][x]['validTime'][0:10])
+        print('Time: ' + current_weather['timeSeries'][x]['validTime'][11:16])
+
+        # Get symbols for the weather.
         for weather in weather_desc['weathers']:
             if current_weather['timeSeries'][x]['parameters'][18]['values'][0] == weather['value']:
                 print('Weather: ' + weather['name'] + '  ' + weather['symbol'])
